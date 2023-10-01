edf841e5e028ff4b8e66d5048600ca79244dd488
New <New@KOMPUTER>
2023-10-02 02:09:30 +0300

    Добавьте файлы проекта.

---
+++ PasswordLibrary/PasswordClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PasswordLibrary
+{
+    public class PasswordClass
+    {
+        /// <summary>
+        /// проверка сложности пароля
+        /// </summary>
+        /// <param name="password">пароль</param>
+        /// <returns>
+        /// числовое значение сложности пароля
+        /// </returns>
+        public static int PasswordStrengthCheker(string password)
+        {
+            if (String.IsNullOrEmpty(password))
+            {
+                return 0;
+            }
+            int result = 0;
+
+            // если пароль имеет длину больше 7 символов сложность повышается на 1 балл
+            if (password.Length>7)
+            {
+                result++;
+            }
+            //если пароль содержит цифры, то сложность повышается ещё на 1 балл
+            if (Regex.Match(password, "[0-9]").Success)
+            {
+                result++;
+            }
+            //если пароль содержит латинские буквы в нижнем регистре, то сложность повышается ещё на 1 балл
+            if (Regex.Match(password, "[a-z]").Success)
+            {
+                result++;
+            }
+            //если пароль содержит Заглавные латинские буквы, то сложность повышается ещё на 1 балл
+            if (Regex.Match(password, "[A-Z]").Success)
+            {
+                result++;
+            }
+            //если пароль содержит спецсимволы, то сложность повышается ещё на 1 балл
+            if (Regex.Match(password, "[\\!\\@\\#\\$\\%\\^\\&\\*\\(\\)\\-\\=\\_\\+\\[\\]\\{\\}\\;\\:\\'\\,\\.]").Success)
+            {
+                result++;
+            }
+            //если пароль содержит кириллические символы,
+            //то выдается исключение «Кириллические символы запрещены при вводе пароля»
+            if (Regex.Match(password, "[а-яА-ЯёЁ]").Success)
+            {
+                throw new Exception("Кириллические символы запрещены при вводе пароля");
+            }
+
+            return result;
+
+        }
+    }
+}
---
+++ PasswordLibraryTests/PasswordStrengthCheker.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using PasswordLibrary;
+
+namespace PasswordLibraryTests
+{
+    [TestClass]
+    public class PasswordStrengthCheker
+    {
+        /// <summary>
+        /// Верних регистр 1, нижний регистр 1, цифра 1, спецсимвол 1, длина строки > 7 (сложность 5)
+        /// </summary>
+        [TestMethod]
+        public void PasswordStrengthCheker_AllChars_5Points()
+        {
+            //arrange
+            string password = "P2ssw0rd#";
+            int expected = 5;
+            //act
+            int actual = PasswordClass.PasswordStrengthCheker(password);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        /// Верних регистр 1, нижний регистр 1, за длину строки 1 (сложность 4)
+        /// </summary>
+        [TestMethod]
+        public void PasswordStrengthCheker_UpperCase_5Points()
+        {
+            //arrange
+            string password = "Password";
+            int expected = 4;
+            //act
+            int actual = PasswordClass.PasswordStrengthCheker(password);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        /// Верних регистр 1, нижний регистр 1, за длину строки 1, цифра 1 (сложность 4)
+        /// </summary>
+        [TestMethod]
+        public void PasswordStrengthCheker_ContainsNumbers_4Points()
+        {
+            //arrange
+            string password = "Passw0ord";
+            int expected = 4;
+            //act
+            int actual = PasswordClass.PasswordStrengthCheker(password);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        /// Верних регистр 1, нижний регистр 1, за длину строки 1, цифра 1, спецсимвол 1 (сложность 5)
+        /// </summary>
+        [TestMethod]
+        public void PasswordStrengthCheker_ContainsSpecialChar_4Points()
+        {
+            //arrange
+            string password = "Passw0rd@";
+            int expected = 5;
+            //act
+            int actual = PasswordClass.PasswordStrengthCheker(password);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        /// пустая строка
+        /// </summary>
+        [TestMethod]
+        public void PasswordStrengthCheker_EmptyString_4Points()
+        {
+            //arrange
+            string password = String.Empty;
+            int expected = 0;
+            //act
+            int actual = PasswordClass.PasswordStrengthCheker(password);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        /// криллица
+        /// </summary>
+        [TestMethod]
+        public void PasswordStrengthCheker_KirillicaString_4Points()
+        {
+            //arrange
+            string password = "Акуьплоуткь";
+
+            //act
+            Action actual = () => new PasswordClass().PasswordStrengthCheker(password);
+            //assert
+            Assert.ThrowsException<ArgumentException>(actual);
+        }
+    }
+}
